6c6b7a04b784165c2ea147d5445a161b4785d4da
f2m2rd <rocky@f2m2.com>
2017-10-22 08:41:57 -0700

    Update AmazonAlexaDriver.php

--- src/AmazonAlexaDriver.php
+++ src/AmazonAlexaDriver.php
@@ -74,11 +74,9 @@ class AmazonAlexaDriver extends HttpDriver
             $session = $this->payload->get('session');
 
             $message = new IncomingMessage($intent['name'], $session['user']['userId'], $session['sessionId']);
-            
             if (array_key_exists('slots', $intent)) {
                 $message->addExtras('slots', Collection::make($intent['slots']));
             }
-            
             $this->messages = [$message];
         }
 
